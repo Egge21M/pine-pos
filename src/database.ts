5e2213cd9390a53411b52da685ff4a079f39ee1f
Egge <egge@getcurrent.io>
2024-08-27 20:57:26 +0200

    refactored entry file

---
+++ src/database.ts
@@ -0,0 +1,21 @@
+import { Pool } from "pg";
+import { OrderStore } from "./store/OrderStore";
+import { PaymentStore } from "./store/PaymentStore";
+
+class PostgresAdapter {
+  pool: Pool;
+
+  constructor() {
+    this.pool = new Pool();
+  }
+
+  async query(query: string, args?: any[]) {
+    return this.pool.query(query, args);
+  }
+}
+
+export function initDataStore() {
+  const db = new PostgresAdapter();
+  OrderStore.getInstance(db);
+  PaymentStore.getInstance(db);
+}
--- src/index.ts
+++ src/index.ts
@@ -1,41 +1,9 @@
-import { Pool } from "pg";
+import { initDataStore } from "./database";
 import app from "./server";
-import { OrderStore } from "./store/orderStore";
-import { PaymentStore } from "./store/PaymentStore";
 
-const port = 8000;
+const port = process.env.PORT || 8000;
 
-class PostgresAdapter {
-  pool: Pool;
-
-  constructor() {
-    this.pool = new Pool();
-  }
-
-  async query(query: string, args?: any[]) {
-    return this.pool.query(query, args);
-  }
-}
-
-async function test() {
-  const order = await OrderStore.getInstance().createOrder(1, "sat");
-  console.log("");
-  console.log("Order created:");
-  console.log(order);
-  console.log("");
-  console.log("=============");
-  console.log("");
-  const payment = await PaymentStore.getInstance().getPaymentById(
-    order.paymentId,
-  );
-  console.log(payment);
-}
-
-const db = new PostgresAdapter();
-OrderStore.getInstance(db);
-PaymentStore.getInstance(db);
-
-test();
+initDataStore();
 
 app.listen(port, () => {
   console.log("Sever started on port: ", port);
