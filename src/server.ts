361bb0fdd572383f633a09f054da5fb1f5eb6837
Egge <egge@getcurrent.io>
2024-08-27 20:56:27 +0200

    added cors

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,13 @@
 import Express from "express";
 import bodyParser from "body-parser";
 import { apiRouter } from "./router";
+import cors from "cors";
 
 const app = Express();
 
 app.use(bodyParser.json());
+app.use(cors());
+
 app.use("/api/v1", apiRouter);
 
 export default app;
